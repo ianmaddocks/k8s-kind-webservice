795b8108a92c614e20968c87b134d3f2e0a50d9a
ianmaddocks <ian@maddocks.name>
2022-05-03 17:01:15 +0100

    changed url

--- demo.sh
+++ demo.sh
@@ -8,5 +8,5 @@ sleep 1
 terraform apply -auto-approve
 
 sleep 2
-printf "\nYou should see '/home' as a reponse below:\n"
-curl http://localhost/home
+printf "\nYou should see '/version' as a reponse below:\n"
+curl http://localhost/version
